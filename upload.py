bd6df6a100b71b51c34426c3e1318df53de639b5
jakemagner-alation <jake.magner@alation.com>
2014-09-06 19:07:43 -0700

    add new notebooks to list of existing ones

--- upload.py
+++ upload.py
@@ -45,7 +45,9 @@ class EvernoteUploader(object):
 
     def save_notebook_return_guid(self, notebook):
         if notebook.name not in self.notebook_guid_by_name:
-            return self.note_store.createNotebook(notebook).guid
+            notebook = self.note_store.createNotebook(notebook)
+            self.notebook_guid_by_name[notebook.name] = notebook.guid
+            return notebook.guid
         return self.notebook_guid_by_name[notebook.name]
 
     def find_note(self, note):
@@ -59,6 +61,7 @@ class EvernoteUploader(object):
         return None
 
     def create_note(self, file_path, notebook_guid):
+        note = ttypes.Note()
         title = ntpath.basename(file_path)
         resource = self.create_resource_from_file(file_path)
         hash_hex = binascii.hexlify(resource.data.bodyHash)
@@ -71,13 +74,11 @@ class EvernoteUploader(object):
             '"http://xml.evernote.com/pub/enml2.dtd">'
         content += '<en-note>'
         content += '<en-media type="' + resource.mime + '" hash="' + hash_hex + '"/>'
-        content += '</en-note>' 
-        note = ttypes.Note(
-            title=title,
-            content=content,
-            notebookGuid=notebook_guid,
-            resources=[resource]
-        )
+        content += '</en-note>'
+        note.resources = [resource]
+        note.title = title
+        note.content = content
+        note.notebookGuid = notebook_guid
         return note
 
     def save_note(self, note):
